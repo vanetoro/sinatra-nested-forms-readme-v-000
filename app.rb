1412f367e74d2a9e27736745ff0df459daf8d4bd
Vanessa Toro <vanetoro84@gmail.com>
2018-05-23 17:22:11 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/new' do
+  get '/' do
     erb :index
   end
 
--- models/course.rb
+++ models/course.rb
@@ -1,14 +1,14 @@
 class Course
   attr_reader :name, :topic
- 
+
   COURSES = []
- 
+
   def initialize(args)
     @name = args[:name]
     @topic = args[:topic]
     COURSES << self
   end
- 
+
   def self.all
     COURSES
   end
